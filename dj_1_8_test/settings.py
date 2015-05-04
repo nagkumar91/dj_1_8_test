db1e04f4ebdfa2c8b0c9935e1a0feb747e9e2c1c
Nagkumar <me@nagkumar.com>
2015-05-03 23:53:01 -0700

    removed pyc and added necessary settings

--- dj_1_8_test/settings.py
+++ dj_1_8_test/settings.py
@@ -101,3 +101,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#mandatory for hosting in heroku
+MEDIA_ROOT = 'media'
+STATIC_ROOT = 'static'
